7250294b651d4a79beb0d551bc6f2ed485da151c
Ruchita Makwana <ruchitahm@gmail.com>
2023-01-08 18:18:26 -0500

    Mongoose Part 3

--- mongoose/models/dishes.js
+++ mongoose/models/dishes.js
@@ -1,17 +1,36 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating: {
+        type: Number,
+        min:1,
+        max: 5,
+        require: true
+    },
+    comment: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: String,
+        required: true,
+    }
+},{
+    timestamps: true
+})
+
 const dishSchema = new Schema({
     name: {
-        type: String,
-        // _id: String,
+        type: String, 
         required: true,
         unique: true
     },
     description: {
         type: String,
         required: true
-    }
+    },
+    comments: [commentSchema]
 },{
     timestamps: true
 });
