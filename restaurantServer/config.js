70f7b2de4894e44809c12515b18c5fec478e89f0
Ruchita Makwana <ruchitahm@gmail.com>
2023-01-11 17:28:39 -0500

    Passport Facebook

--- restaurantServer/config.js
+++ restaurantServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/restaurant'
+    'mongoUrl' : 'mongodb://localhost:27017/restaurant',
+    'facebook' : {
+        clientId: '674895857663693',
+        clientSecret: 'd9dacb6008bb85c49811d51c224dab17'
+    }
 }
